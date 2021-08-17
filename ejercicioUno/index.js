d2e92bb4402bd9013e51602236dc6c8606bf198f
Victoria Diaz <diazmariavictoria94@gmail.com>
2021-08-17 14:53:50 -0300

    ejercicio uno finalizado

---
+++ ejercicioUno/index.js
@@ -0,0 +1,9 @@
+//Mostrar en consola
+
+//Dado un array de 10 números, mostrar cada uno en consola.
+
+const numeros = [1, 5, 8, 7, 15, 9, 98, 2, 4, 6];
+
+for (i = 0; i < numeros.length; i++) {
+	console.log(numeros[i]);
+}
