2dbfc1580acbadb9942fd33d8a5d8f806f22c6ab
Victoria Diaz <diazmariavictoria94@gmail.com>
2021-08-20 11:47:09 -0300

    hola

---
+++ ejercicioCinco/index.js
@@ -0,0 +1,14 @@
+//Promedio de un curso 2
+
+//Dado un array de cualquier cantidad de numeros, donde cada número representa la nota de una alumna, retornar el promedio final de nota del curso. (Nota: recordá que podes saber la longitud de un array con array.length)
+
+const numeros = [5, 5, 5];
+
+promedioNotas = (nota) => {
+	sumarNotas = 0;
+	for (let i = 0; i < nota.length; i++) {
+		sumarNotas++;
+	}
+};
+
+console.log(promedioNotas(numeros));
