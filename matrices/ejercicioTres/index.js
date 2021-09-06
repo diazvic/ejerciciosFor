c57262c0c7d55caa168190abde75042072fb9b7e
Victoria Diaz <diazmariavictoria94@gmail.com>
2021-09-06 15:06:10 -0300

    ejercicio terminado

---
+++ matrices/ejercicioTres/index.js
@@ -0,0 +1,18 @@
+// Alumna más grande
+
+// Dado un array bidimensional compuesto de dos arrays de 5 elementos cada uno, donde el primero esta compuesto de nombres y el segundo de edades, retornar el nombre de la persona con la mayor edad.
+
+const nombresEdades = [
+	["Euge", "Guadi", "Fran", "Santi", "Moni"],
+	["25", "45", "10", "8", "96"],
+];
+mostrarPersonaMayor = (nombresEdades) => {
+	for (let i = 0; i < nombresEdades.length; i++) {
+		const nombre = nombresEdades[i];
+		console.log(nombre);
+		for (let j = 0; j < nombre.length; j++) {
+			if (nombre[j]) {
+			}
+		}
+	}
+};
